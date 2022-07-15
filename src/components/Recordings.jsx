8cd76d17270e166ef63dd0c04246b9fa42263ec6
khasanjonovich <khasanjonovich@gmail.com>
2022-07-15 15:15:25 +0900

    wavesurfer.js not showing

--- src/components/Recordings.jsx
+++ src/components/Recordings.jsx
@@ -2,17 +2,47 @@ import useRecordingsList from "../hooks/useRecordingsList";
 import "./recordings.css";
 import { MdDelete } from "react-icons/md";
 import { Howl } from "howler";
+import WaveSurfer from "wavesurfer.js";
+import { useLayoutEffect, useRef } from "react";
 
 export default function RecordingsList({ audio, timestamps }) {
   const { recordings, deleteAudio } = useRecordingsList(audio);
+  const waveformRef = useRef();
+  const trackRef = useRef();
 
   const howler = new Howl({
     src: audio,
     format: ["mp3", "webm"],
     sprite: timestamps,
   });
-  console.log(howler);
-  console.log(timestamps);
+
+  useLayoutEffect(() => {
+    console.log(waveformRef.current);
+    if (waveformRef.current) {
+      console.log(waveformRef);
+
+      const wavesurfer = WaveSurfer.create({
+        barWidth: 3,
+        cursorWidth: 1,
+        container: waveformRef.current,
+        backend: "WebAudio",
+        height: 80,
+        progressColor: "#2D5BFF",
+        responsive: true,
+        waveColor: "#EFEFEF",
+        cursorColor: "transparent",
+      });
+      console.log(recordings);
+      console.log(audio);
+      wavesurfer.load("https://www.mfiles.co.uk/mp3-downloads/gs-cd-track2.mp3");
+      wavesurfer.on("ready", function () {
+        console.log("ready");
+        wavesurfer.play();
+      });
+    }
+
+    return () => {};
+  }, []);
 
   return (
     <div className="recordings-container">
@@ -23,9 +53,10 @@ export default function RecordingsList({ audio, timestamps }) {
               {item}
             </div>
           ))}
-          {recordings.map((record) => (
+          {recordings.map((record, i) => (
             <div className="record" key={record.key}>
-              {/* <audio controls src={record.audio} /> */}
+              <div ref={waveformRef}></div>
+              <audio ref={trackRef} src={record.audio} />
               <div className="delete-button-container">
                 <MdDelete className="delete-button" title="Delete this audio" onClick={() => deleteAudio(record.key)}></MdDelete>
               </div>
